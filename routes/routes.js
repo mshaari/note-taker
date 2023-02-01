34c1501e88ab164bcb2e05a50053dc41b84bd286
mshaari <michael.shaari@gmail.com>
2023-02-01 12:17:21 -0500

    Mostly functional code, altering routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 const path = require('path');
+var uniqueId = require('generate-unique-id');
 
 module.exports = app => {
-    fs.writeFile("db/db.json", 'utf8', (err, data) => {
+    fs.readFile("db/db.json", 'utf8', (err, data) => {
 
         if (err) throw (err);
 
         var notes = JSON.parse(data);
 
         function refreshDb() {
-            fs.writeFile('db/db.json', JSON.stringify(notes, null, '\t'), (err) => {
+            fs.writeFile('./db/db.json', JSON.stringify(notes, null, '\t'), (err) => {
                 if (err) throw (err);
                 return true;
             });
@@ -17,31 +18,43 @@ module.exports = app => {
 
         app.get('/api/notes', function(req, res) {
             res.json(notes);
+
+            refreshDb();
+
+            // Log our request to the terminal
+            console.info(`${req.method} request received to get reviews`);
         });
 
-        app.post('api/notes', function(req, res) {
-            var newNote = req.body;
+        app.post('/api/notes', function(req, res) {
+            var newNote = {
+                title: req.body.title,
+                text: req.body.text,
+                id: uniqueId(), //Use npm package
+              };
+            console.log(newNote);
             notes.push(newNote); //is this how we add it to the json file?? which package do we use to assign the unique id to each note?
             refreshDb();
+            res.json(notes);
         });
 
-        app.post('api/notes/:id', function(req, res) {
+        app.get('/api/notes/:id', function(req, res) {
             res.json(notes[req.params.id]); //captures the id parameters from our request and goes to that element in the notes array
         });
 
-        app.delete('api/notes/:id', function(req, res) {
+        app.delete('/api/notes/:id', function(req, res) {
             notes.splice(req.params.id, 1); //WHAT DOES THIS MEAN?
             refreshDb();
+            res.json(notes);
             console.log(`Note with id = ${req.params.id} has been deleted.`);
         });
         
         app.get('/notes', function(req, res) {
-            res.sendFile(path.join(__dirname, './public/notes.html'));
+            res.sendFile(path.join(__dirname, '../public/notes.html'));
         });
 
         //this has to go at the bottom, right
         app.get('*', function(req, res) {
-            res.sendFile(path.join(__dirname, './public/index.html'));
+            res.sendFile(path.join(__dirname, '../public/index.html'));
         });
 
     });
--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const path = require('path');
 const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-
-// Link to the routes file
-require('./routes/routes')(app);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// Link to the routes file
+require('./routes/routes.js')(app);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
